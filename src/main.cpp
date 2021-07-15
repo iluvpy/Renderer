ea6570b2d374ae42406f98322887420d872ed22b
iluvpy <miomanns265@gmail.com>
2021-07-15 17:26:31 +0200

    replaced code with Classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <filesystem>
+
 #include "glad/glad.h"
 #include <GLFW/glfw3.h>
+
+
+#include "lib/Shader.hpp"
+#include "lib/VertexBuffer.hpp"
+
 
 int main(int argc, char **argv) {
 	GLFWwindow* window;
@@ -13,7 +20,7 @@ int main(int argc, char **argv) {
 
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(920, 780, "Renderer", NULL, NULL);
     if (!window)
     {	
 		std::cout << "could not create glfw window\n";
@@ -21,10 +28,12 @@ int main(int argc, char **argv) {
         return -1;
     }
 
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
 	/* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+	// load all OpenGL function after creating GL context
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     	std::cout << "Failed to initialize GLAD" << std::endl;
 		glfwTerminate();
@@ -32,33 +41,25 @@ int main(int argc, char **argv) {
 	}
 	
 	float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
+     -0.5f, -0.5f, 0.0f,
      0.5f, -0.5f, 0.0f,
      0.0f,  0.5f, 0.0f
 	};  
 
-	GLuint buffer;
-	glGenBuffers(1, &buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	VertexBuffer buffer(vertices);
+	Shader orange("./shaders/fragmentShader.glsl", "./shaders/vertexShader.glsl");
 
-	const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-	
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
 		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+		orange.bind();
+		buffer.bind();
+		buffer.draw();
+		buffer.unbind();
+		orange.unbind();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
