47fe172529691f35d12af6ec675ba6c1a001cbc6
iluvpy <miomanns265@gmail.com>
2021-07-25 23:49:16 +0200

    changed Name

--- src/include/ColorShader.hpp
+++ src/include/ColorShader.hpp
@@ -2,17 +2,18 @@
 
 #include "Shader.hpp"
 
-class Color {
+class ColorShader {
 public:
-	Color(float r=0, float g=0, float b=0, float a=255);
+	ColorShader(float r=255.0f, float g=255.0f, float b=255.0f, float a=255.0f);
 	void bind();
 	void unbind();
 	void setR(float _r);
 	void setG(float _g);
 	void setB(float _b);
 	void setA(float _a);
-
-	~Color();
+	inline const Shader& getShader() { return colorShader; }
+	inline const GLuint getShaderProgram() { return colorShader.getProgram(); }
+	~ColorShader();
 private:
 
 	void from255to1(float r, float g, float b, float a);
