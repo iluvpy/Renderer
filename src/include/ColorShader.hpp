6785b6205a6dc32dd5ca79e45342fc4893537b3d
iluvpy <miomanns265@gmail.com>
2021-07-26 15:05:27 +0200

    created ColorShader

--- src/include/ColorShader.hpp
+++ src/include/ColorShader.hpp
@@ -1,23 +1,15 @@
 #pragma once
 
 #include "Shader.hpp"
+#include "Color.hpp"
 
-class ColorShader {
+class ColorShader : public Shader {
 public:
-	ColorShader(float r=255.0f, float g=255.0f, float b=255.0f, float a=255.0f);
-	void bind();
-	void unbind();
-	void setR(float _r);
-	void setG(float _g);
-	void setB(float _b);
-	void setA(float _a);
-	inline const Shader& getShader() { return colorShader; }
-	inline const GLuint getShaderProgram() { return colorShader.getProgram(); }
+	ColorShader();
+	ColorShader(const Color& c);
+	
 	~ColorShader();
 private:
 
-	void from255to1(float r, float g, float b, float a);
-	float r, g, b, a;
-	Shader colorShader;
-	int location;
+	Color c;
 };
