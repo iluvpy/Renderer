50e65cb3062b327b77f33754f2f21aeec13b586d
iluvpy <miomanns265@gmail.com>
2021-07-26 15:04:57 +0200

    refactored shader

--- src/include/BufferLayout.cpp
+++ src/include/BufferLayout.cpp
@@ -3,41 +3,32 @@
 
 BufferLayout::BufferLayout() {}
 
-BufferLayout::BufferLayout(const VertexBuffer& vb, const IndexBuffer& ib) {
-	init(vb, ib);
-}
-
-void BufferLayout::init(const VertexBuffer& vb, const IndexBuffer& ib) {
+BufferLayout::BufferLayout(const VertexBuffer& vb, const IndexBuffer& ib, const Shader& sh) {
 	m_vb = vb;
 	m_ib = ib;
+	m_shader = sh;
 }
 
+
 void BufferLayout::bind() {
-	if (shaderType == ShaderType::SHADER) shader.bind();
-	else if (shaderType == ShaderType::COLOR) color.bind();
+	m_shader.bind();
 	m_vb.bind();
 	m_ib.bind();
 }
 void BufferLayout::unbind() {
 	m_vb.unbind();
 	m_ib.unbind();
-	if (shaderType == ShaderType::SHADER) shader.unbind();
-	else if (shaderType == ShaderType::COLOR) color.unbind();
+	m_shader.unbind();
+}
+
+void BufferLayout::setShader(const Shader& _shader) {
+	m_shader = _shader;
 }
 
 void BufferLayout::draw() {
 	bind();
 	glDrawElements(GL_TRIANGLES, m_ib.getCount(), GL_UNSIGNED_INT, nullptr);
 	unbind();
-}
-
-void BufferLayout::setShader(const Shader& _shader) { 
-	shader = _shader; 
-	shaderType = ShaderType::SHADER;
-}
-void BufferLayout::setColor(const ColorShader& c) { 
-	color = c; 
-	shaderType = ShaderType::COLOR;
 }
 
 BufferLayout::~BufferLayout() {}
--- src/include/BufferLayout.hpp
+++ src/include/BufferLayout.hpp
@@ -2,29 +2,24 @@
 
 #include "VertexBuffer.hpp"
 #include "IndexBuffer.hpp"
-#include "Shader.hpp"
 #include "ColorShader.hpp"
+#include "Color.hpp"
 #include "types.hpp"
 
 
 class BufferLayout {
 public:
 	BufferLayout();
-	BufferLayout(const VertexBuffer& vb, const IndexBuffer& ib);
-	void init(const VertexBuffer& vb, const IndexBuffer& ib);
+	BufferLayout(const VertexBuffer& vb, const IndexBuffer& ib, const Shader& sh);
 
-	void setColor(const ColorShader& c);
 	void setShader(const Shader& _shader);
 	void bind();
 	void unbind();
 	void draw();
 	~BufferLayout();
 private:
-	
+
 	VertexBuffer m_vb;
 	IndexBuffer m_ib;
-	Shader shader;
-	ColorShader color;
-	ShaderType shaderType=ShaderType::NONE;
-
+	Shader m_shader;
 };
