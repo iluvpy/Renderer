8a551c67d06e79bf52184eaab347ae5921c32cd8
iluvpy <miomanns265@gmail.com>
2021-07-27 17:18:33 +0200

    added simple color Shader

--- src/include/ColorShader.cpp
+++ src/include/ColorShader.cpp
@@ -1,1 +1,20 @@
 #include "ColorShader.hpp"
+
+
+ColorShader::ColorShader(float _r, float _g, float _b, float _a) 
+: r(_r/255), g(_g/255), b(_b/255), a(_a/255)
+{
+	m_shader.init(U_COLOR_FSH, U_COLOR_VSH);
+	m_location = m_shader.getUniformLocation("u_Color");
+}
+
+void ColorShader::bind() {
+	m_shader.bind();
+	glUniform4f(m_location, r, g, b, a);
+}
+
+void ColorShader::unbind() {
+	m_shader.unbind();
+}
+
+ColorShader::~ColorShader() {}
--- src/include/ColorShader.hpp
+++ src/include/ColorShader.hpp
@@ -1,15 +1,23 @@
 #pragma once
 
 #include "Shader.hpp"
-#include "Color.hpp"
 
-class ColorShader : public Shader {
+class ColorShader {
 public:
-	ColorShader();
-	ColorShader(const Color& c);
-	
+	ColorShader(float r=255.0f, float g=255.0f, float b=255.0f, float a=255.0f);
+	void bind();
+	void unbind();
+	inline void setR(float _r) { r = _r/255.0f; }
+	inline void setG(float _g) { g = _g/255.0f; }
+	inline void setB(float _b) { b = _b/255.0f; }
+	inline void setA(float _a) { a = _a/255.0f; }
+	inline float getR() { return r;}
+	inline float getG() { return g;}
+	inline float getB() { return b;}
+	inline float getA() { return a;}
 	~ColorShader();
 private:
-
-	Color c;
+	Shader m_shader;
+	uint m_location;
+	float r, g, b, a;
 };
