d28e6d561b5cfcc8ccc74cc77cf3457fa37c2fe4
iluvpy <miomanns265@gmail.com>
2021-08-11 23:23:24 +0200

    changed vec4 to vec2

--- shaders/basicVs.glsl
+++ shaders/basicVs.glsl
@@ -1,12 +1,12 @@
 #version 330 core
 
-layout (location = 0) in vec4 aPos;
+layout (location = 0) in vec2 aPos;
 layout (location = 1) in vec3 aColor;
 
 uniform mat4 u_Proj;
 out vec3 v_Color; // vertex color
 void main()
 {   
-    gl_Position = u_Proj * aPos;
+    gl_Position = u_Proj * vec4(aPos.x, aPos.y, 1.0, 1.0);
     v_Color = aColor;
 }
